6b3e070ad23ac774992c385df639c0941476e1f0
d-95 <dwayne.dyer@gmail.com>
2021-08-10 12:37:09 +0100

    you're doing great sweetie

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("the life of a salaryman is...")
+	x := 3453 + 3435
+	fmt.Println(x)
+	fmt.Println("the life of a salaryman is...")
+	y := 345 + 2349
+	fmt.Println(y)
+	x = 1
+	fmt.Println(x)
 }
